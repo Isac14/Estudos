7cdde52d0d4b670e281890ecd58b5215fcabc1a0
Isac <isac.mamede14@gmail.com>
2023-10-06 16:58:14 -0300

    solved exercise

--- DOM/Exercício 2/script.js
+++ DOM/Exercício 2/script.js
@@ -1,18 +1,25 @@
+// Seleção de elementos
 const inputPalavra = document.querySelector("#palavra");
 const lista = document.querySelector("#lista");
 const palavras = lista.querySelectorAll("li");
 var listaPalavras = [];
 
+// Inserindo palavras da lista em um array
 palavras.forEach((item) => {
   listaPalavras.push(item.innerText);
 });
 
 console.log(listaPalavras);
 
+// Comparando input com itens da lista
 inputPalavra.addEventListener("input", function () {
-  console.log(inputPalavra.value);
   for (let x = 0; x < listaPalavras.length; x++) {
-    if (inputPalavra.value == listaPalavras[x]) {
+    if (
+      inputPalavra.value != "" &&
+      listaPalavras[x]
+        .toLowerCase()
+        .startsWith(inputPalavra.value.toLowerCase())
+    ) {
       palavras[x].setAttribute("class", "negrito");
     } else {
       palavras[x].classList.remove("negrito");
